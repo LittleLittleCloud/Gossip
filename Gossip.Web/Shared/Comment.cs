a7c7db173acc8546dbde8e23a597c47e9f7cae96
XiaoYun Zhang <xiaoyuz@microsoft.com>
2022-06-08 14:27:25 -0700

    update

--- Gossip.Web/Shared/Comment.cs
+++ Gossip.Web/Shared/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Gossip.Core
 {
     public class Comment
     {
-        public string ID { get; set; }
+        public long ID { get; set; }
 
         public long CreatedTimestampInSeconds { get; set; }
 
